647635c0b1e30fa8a92b2e3063dfb835279e2af7
nicolasdejesus653 <nicolasjsilva622@gmail.com>
2026-03-25 23:22:43 -0300

    Create tables for authors and books with sample data

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,28 @@
+CREATE TABLE autores (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
 
+CREATE TABLE livros (
+    id INT PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    isbn VARCHAR(20),
+    ano_publicacao INT,
+    autor_id INT,
+    CONSTRAINT fk_autor
+    FOREIGN KEY (autor_id)
+    REFERENCES autores(id
+);
+
+INSERT INTO autores (id, nome)
+VALUES (1, 'Machado de Assis');
+
+INSERT INTO livros (id, titulo, isbn, ano_publicacao, autor_id)
+VALUES (1, 'Dom Casmurro', '123456789', 1899, 1)
+
+SELECT titulo
+FROM livros ;
+
+SELECT *
+FROM livros
+WHERE ano_publicacao > 2020;
